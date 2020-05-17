5a6f05dab6c5cb22e6dbeaee6b72abf0d540f992
roxyvaught <roxanna.vaught@gmail.com>
2020-05-17 13:10:50 -0500

    refractor and add style

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 const inquirer = require('inquirer');
+
 const fs = require('fs');
-// const generatePage = require('./src');
 const generatePage = require('./src/page-template.js');
-// const profileDataArgs = process.argv.slice(2);
-// const pageHTML = generatePage(portfolioData);
-//const [name, github] = profileDataArgs;
+const {writeFile, copyFile} = require('./utils/generate-site.js');
 
-// fs.writeFile('./index.html', generatePage(name, github), err => {
-//  if (err) throw new Error(err);
-
-//  console.log('Portfolio complete! Check out index.html to see the output!');
-// });
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -135,11 +128,18 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-     const pageHTML = generatePage(portfolioData);
-
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
